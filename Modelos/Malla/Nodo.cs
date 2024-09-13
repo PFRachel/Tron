0d9495cd585872af1c28bf5965acc96f604cb5ab
Crazymili <Crazymili@gmail.com>
2024-09-13 12:11:59 -0600

    version10

--- Modelos/Malla/Nodo.cs
+++ Modelos/Malla/Nodo.cs
@@ -4,13 +4,13 @@ namespace JuegoTron // espacio nombre del juego
 {
     public class Node// clase nodo
     {
-        public PictureBox CuadroImagen{get; set;} //cuadro donde se dibuja
+        public PictureBox? CuadroImagen{get; set;} //cuadro donde se dibuja
 
-        public Node Arriba{get; set;}// Nodo conectado de arriba
-        public Node Abajo {get; set;}//Nodo conectado abajo
+        public Node? Arriba{get; set;}// Nodo conectado de arriba
+        public Node? Abajo {get; set;}//Nodo conectado abajo
 
-        public Node Izquierda{get; set;}//Nodo conectado derecha
-        public Node Derecha{get; set;}//Nodo conectado derecha
+        public Node? Izquierda{get; set;}//Nodo conectado derecha
+        public Node? Derecha{get; set;}//Nodo conectado derecha
 
     }
 
--- Modelos/Moto/Moto.cs
+++ Modelos/Moto/Moto.cs
@@ -7,33 +7,49 @@ namespace JuegoTron
 {
     public class MotoNodo
     {
-        public Node GridNode { get; set; } // El nodo del grid donde se encuentra esta parte de la moto
-        public MotoNodo Next { get; set; } // El siguiente nodo en la estela
+        public Node? GridNode { get; set; } // El nodo del grid donde se encuentra esta parte de la moto
+        public MotoNodo? Next { get; set; } // El siguiente nodo en la estela
+        public Node? Position {get; set; }// El nodo donde esta esa parte de la moto
     }
     public class ListaEnlazadaMoto
     {
         public MotoNodo Head { get; private set; }
         public MotoNodo Tail { get; private set; }
         public int EstelaMaxima { get; private set; }
-
+        public Direction CurrentDirection  { get; set; }
+        /// 
+        public Node? PosicionActual {get; private set;}//Posicion actual
+        public int Direccion {get; set; } //0 arriba, 1 derecha, 2 abajo, 3 izquierda 
+        public int Combustible { get;set; }
+        public bool Vivo = true;
+        public Color colorMoto = Color.Red;
+        public Color colorEstelaMoto = Color.LightCoral;
         public ListaEnlazadaMoto()
         {
             this.EstelaMaxima = 4;
         }
-
+        public void RecargarCombustible(int cantidad)
+        {
+            Combustible += cantidad;
+            if (Combustible > 100)
+            {
+                Combustible = 100; // Limitar el combustible a 100
+            }
+        }
         // Método para añadir un nodo al inicio de la lista
         public void Add(Node gridNode)
         {
             // Si ya hay una cabeza, actualizamos su imagen a la de la estela
             if (Head != null)
             {
-                Head.GridNode.CuadroImagen.BackColor = Color.White;
+                Head.GridNode.CuadroImagen.BackColor = colorEstelaMoto;
             }
 
-            MotoNodo newNodo = new MotoNodo { GridNode = gridNode };
+            MotoNodo newNodo = new MotoNodo { GridNode = gridNode, Position = gridNode };
             if (Head == null)
             {
                 Head = Tail = newNodo;
+                PosicionActual = gridNode;
             }
             else
             {
@@ -42,8 +58,22 @@ namespace JuegoTron
             }
 
             // Actualizar la imagen del nuevo nodo al de la moto
-            gridNode.CuadroImagen.BackColor = Color.Violet;
+            gridNode.CuadroImagen.BackColor = colorMoto;
         }
+        public void MoverConCombustible(Node newGridNode)
+        {
+            if (Combustible > 0)
+            {
+                Move(newGridNode);
+                Combustible -= 1; // Decrece el combustible
+            }
+            else
+            {
+                
+                MessageBox.Show("Se acabó el combustible, ¡has perdido!");
+            }
+        }
+
 
         // Método para mover la moto, manteniendo la longitud de la estela
         public void Move(Node newGridNode)
@@ -66,6 +96,24 @@ namespace JuegoTron
                 current = current.Next;
                 count++;
             }
+            PosicionActual = newGridNode;
         }
+        public void AumentarEstela(int cantidad)
+        {
+            EstelaMaxima += cantidad; 
+        }
+        //obtenet el nodo siguiente del movimeinto actual 
+         public Node GetsiguienteNodo()
+        {
+            switch (Direccion)
+            {
+                case 0: return PosicionActual?.Arriba;    
+                case 1: return PosicionActual?.Derecha;  
+                case 2: return PosicionActual?.Abajo;     
+                case 3: return PosicionActual?.Izquierda; 
+                default: return PosicionActual;
+            }
+        }
+
     }
 }
